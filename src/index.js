49e34c454d89144c52000796a0b8de8efe13aaca
Benjamin Christau <benjamin.christau@oyez.fr>
2018-07-15 12:42:34 +0200

    Add Appolo client

--- src/index.js
+++ src/index.js
@@ -1,5 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { ApolloProvider } from "react-apollo";
+import { ApolloClient, InMemoryCache, ApolloLink } from "apollo-boost";
+import { withClientState } from "apollo-link-state";
 import { App } from "components/index";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const cache = new InMemoryCache();
+
+const stateLink = withClientState({
+  cache,
+  defaults: {
+    __typename: "routecache"
+  },
+  resolvers: {
+    Mutation: {}
+  }
+});
+
+const link = ApolloLink.from([stateLink]);
+
+const client = new ApolloClient({
+  link,
+  cache
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
